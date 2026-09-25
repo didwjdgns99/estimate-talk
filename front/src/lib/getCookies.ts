6c809a65409f73c4d53891e9fae1ee81f4bb387e
ky7408 <ky7408@naver.com>
2026-09-25 13:38:22 +0900

    refactor getAuthCookie

--- front/src/lib/getCookies.ts
+++ front/src/lib/getCookies.ts
@@ -1,3 +1,12 @@
+// "use server";
+
+// import { cookies } from "next/headers";
+
+// export async function getAuthCookie() {
+//   const token = (await cookies()).get("token")?.value;
+//   return token ? `token=${token}` : undefined;
+// }
+
 "use server";
 
 import { cookies } from "next/headers";
